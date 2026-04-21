27a6b469ea369c8cc8ad034e22ec7c39a249ee04
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-04-21 15:49:41 +0000

    hello vending

---
+++ vending.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+// separate the variables fom the primt
+//chips
+ printf("snaks vending msachine\n");
+ printf("--------------------------\n");
+ float chips_price1 = $20.5;
+ int chips_quantity1 = 13;
+ char chips_selection code1 = 'c';
+ 
+}
+
+
