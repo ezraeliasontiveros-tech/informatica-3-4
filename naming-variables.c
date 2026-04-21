8d77510793bc3363aecfcf13bda58f5146e5aa5f
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-04-21 14:59:55 +0000

    hello

---
+++ naming-variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+int number; // I fixed the name of the variable deleting the exclamation sign !
+int test_value; // I fixed the name of this variable by changing the space in the word by an underscore
+int example1; // I fixed the name of this variable by changing the position of the number 1 after the example
+float int_count; // I fixed the this variable by adding a name to the variable for example "variables"
+printf("Variables created successfully.\n");
+}
+
