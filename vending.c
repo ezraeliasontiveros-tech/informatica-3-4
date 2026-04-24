e507093dbbfcaa6ea045c996dfb62ae8d391c6b4
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-04-24 16:41:11 +0000

    hello

--- vending.c
+++ vending.c
@@ -6,10 +6,27 @@ int main(void)
 //chips
  printf("snaks vending msachine\n");
  printf("--------------------------\n");
- float chips_price1 = $20.5;
- int chips_quantity1 = 13;
- char chips_selection code1 = 'c';
- 
+printf("chips\n");
+ int Quantity_Chips =20;
+ float chips_price = 20.5;
+ char chips_selection_code = 'c';
+// Soda
+printf("soda\n");
+int Quantity_soda = 10;
+float soda_price = 10.5;
+char soda_selection_code = 'b';
+
+//burgers
+int quantity_burgers = 30;
+float burgers_price = 40;
+char burger_selection_code ='a';
+printf("Quantity_Chips =%d \n",Quantity_Chips);
+printf("chips_price =%f\n",chips_price);
+printf("chips_selection_code =%c\n",chips_selection_code);
+printf("Quantity_soda =%d\n",Quantity_soda);
+printf("soda_price =%f\n",soda_price);
+printf("soda_selection_code =%c\n",soda_selection_code);
+
 }
 
 
