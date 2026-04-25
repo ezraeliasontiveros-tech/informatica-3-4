edb09053e65f00a6a482bcca4a0be64063364c10
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-04-25 01:43:37 +0000

    hello vending

--- vending.c
+++ vending.c
@@ -2,31 +2,14 @@
 
 int main(void)
 {
-// separate the variables fom the primt
-//chips
- printf("snaks vending msachine\n");
- printf("--------------------------\n");
-printf("chips\n");
- int Quantity_Chips =20;
- float chips_price = 20.5;
- char chips_selection_code = 'c';
-// Soda
-printf("soda\n");
-int Quantity_soda = 10;
-float soda_price = 10.5;
-char soda_selection_code = 'b';
-
-//burgers
-int quantity_burgers = 30;
-float burgers_price = 40;
-char burger_selection_code ='a';
-printf("Quantity_Chips =%d \n",Quantity_Chips);
-printf("chips_price =%f\n",chips_price);
-printf("chips_selection_code =%c\n",chips_selection_code);
-printf("Quantity_soda =%d\n",Quantity_soda);
-printf("soda_price =%f\n",soda_price);
-printf("soda_selection_code =%c\n",soda_selection_code);
-
-}
+printf("welcome vending machine \n");
+printf("------------------------\n");
+printf("chips \n");
+float price1 = 25.5;
+int quantity1 = 30;
+char select1 ='A';
+printf("Price = %.2f\n",price1);
+printf("quatity = %d\n",quantity1);
+printf("selection_code = %c\n",select1);
 
 
