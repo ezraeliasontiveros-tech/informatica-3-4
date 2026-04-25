76f813be2dbf0fa181079140df0c5af7935f6f8b
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-04-25 02:22:51 +0000

    rectangle hello

--- rectangle.c
+++ rectangle.c
@@ -3,9 +3,13 @@
 int main(void)
 {
     int length;
+    int width;
     printf("Enter the length of the rectangle: ");
+    scanf("%d", &length);
+    printf("Enter the width of the rectangle ");
     scanf("%d", &width);
     int area = width*length;
     int perimeter = (width+length)*2;
     printf("\nThe area of the rectangle is: %d\nThe perimeter of the rectangle is %d.\n", area, perimeter);
+
 }
