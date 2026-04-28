0a72473e736da9d5fcfd7ada7a73fe1b2a03c99b
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-04-28 15:47:32 +0000

    minutes

---
+++ weekly checkpoint/minutes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+int minutes;
+int start_time;
+printf("Movie running time:\n");
+scanf("%d", &minutes);
+printf("start time:\n");
+scanf("%d", &start_time);
+
+int last;
+int end = (last+start_time);
+printf("the movie will last =%d\n",last);
+printf("End time =%d\n",end);
+
+
+
+
+}
