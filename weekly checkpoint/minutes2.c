3dc85f2c816e2dc1e87d00e4bb13b275737883e4
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-04-29 03:49:55 +0000

    minutes2

--- weekly checkpoint/minutes2.c
+++ weekly checkpoint/minutes2.c
@@ -2,7 +2,23 @@
 
 int main(void)
 {
-    int total;
-    printf("movie running time:\n");
-    scanf("%d", &total);
-    int hours 
+
+
+    int total_minutes;
+    printf("movie running time:");
+    scanf("%d", &total_minutes);
+
+    int hours = total_minutes / 60; //hours
+    int minutes = total_minutes % 60; //minutes
+
+    printf("Equals:%d h %d min\n",hours,minutes);
+
+    int start_h ,start_m;
+    printf("enter start hour:");
+    scanf("%d",&start_h, &start_m);
+
+    int end_h = start_h + hours;
+    int end_m = start_m + minutes;
+    printf("End time: %d:%d\n",end_h,end_m);
+
+}
