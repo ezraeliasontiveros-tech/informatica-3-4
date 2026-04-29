7759d3a7b8280771800df701214bdc737834f1ed
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-04-29 14:57:47 +0000

    receipt

---
+++ test/receipt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+int item;
+printf("item:");
+scanf("%d", &item);
+
+float price;
+printf("price:");
+scanf("%f",&price);
+
+int Quantity_bought;
+printf("Quantity:");
+scanf("%d", &Quantity_bought);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
