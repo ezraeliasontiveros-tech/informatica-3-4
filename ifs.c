c85e1e059676cf850bf3422ac41180b65121f3d3
ezraeliasontiveros-tech <ezraeliasontiveros@gmail.com>
2026-05-06 14:56:37 +0000

    ifs

---
+++ ifs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main (void)
+{
+int a = 5; //equal sign means assigment
+int b = 5;
+int c = 10;
+
+printf("%d == %d is %d \n", a,b,a==b); //Equal sign means comaprison
+printf("%d != %d is %d \n", a,c,a!=c); //!= means "not equal"
+
+//create a progam to check if a number is negative
+int number;
+printf("Enter a number:");
+scanf("%d",&number);
+
+if(number <0){ // si esto es menor a cero
+printf("%d is negative number.\n",number);
+
+} else{ // amenos que
+    printf("%d is positive number.\n",number);
+}
+
+
+}
